a84799de2028fb4125ea9b0ed37bc3ab8d08c0bc
safestak-keith <keith@safestak.com>
2020-09-19 13:07:15 +0200

    Trimmed fields

--- Src/WebApi/PoolStatsPrometheusMetricsSerialiser.cs
+++ Src/WebApi/PoolStatsPrometheusMetricsSerialiser.cs
@@ -16,12 +16,13 @@ namespace SafeStak.Deltas.WebApi
             sb.AppendLine($"safestats_timestamp_updated {stats.StatTimestamp.Ticks}");
             sb.AppendLine($"safestats_pool_active_stake {stats.ActiveStakeLovelaces}");
             sb.AppendLine($"safestats_pool_live_stake {stats.LiveStakeLovelaces}");
-            sb.AppendLine($"safestats_pool_actual_pledge {stats.ActualPledgedLovelaces}");
+            sb.AppendLine($"safestats_pool_lifetime {stats.BlocksLifetime}");
             sb.AppendLine($"safestats_pool_blocks_epoch {stats.BlocksEpoch}");
             sb.AppendLine($"safestats_pool_blocks_epoch_estimate {stats.BlocksEpochEstimate}");
-            sb.AppendLine($"safestats_pool_lifetime {stats.BlocksLifetime}");
             sb.AppendLine($"safestats_pool_delegator_count {stats.DelegatorCount}");
             sb.AppendLine($"safestats_pool_saturated {stats.SaturatedPercentage}");
+            sb.AppendLine($"safestats_pool_pledge {stats.PledgedLovelaces}");
+            sb.AppendLine($"safestats_pool_actual_pledge {stats.ActualPledgedLovelaces}");
 
             return sb.ToString();
         }
--- Src/WebApi/PoolStatsResponse.cs
+++ Src/WebApi/PoolStatsResponse.cs
@@ -4,18 +4,18 @@ namespace SafeStak.Deltas.WebApi
 {
     public class PoolStatsResponse
     {
-        public string PoolId { get; set; }
-        public string Ticker { get; set; }
-        public int RewardsEpoch { get; set; }
         public DateTimeOffset StatTimestamp { get; set; }
-        public long LiveStakeLovelaces { get; set; }
         public long ActiveStakeLovelaces { get; set; }
+        public long LiveStakeLovelaces { get; set; }
+        public int BlocksEpoch { get; set; }
+        public double BlocksEpochEstimate { get; set; }
         public long PledgedLovelaces { get; set; }
         public long ActualPledgedLovelaces { get; set; }
         public int BlocksLifetime { get; set; }
-        public int BlocksEpoch { get; set; }
-        public double BlocksEpochEstimate { get; set; }
         public int DelegatorCount { get; set; }
         public double SaturatedPercentage { get; set; }
+        public string PoolId { get; set; }
+        public string Ticker { get; set; }
+        public int RewardsEpoch { get; set; }
     }
 }
